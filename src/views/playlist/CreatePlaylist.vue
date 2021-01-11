864a8190a53ccd913f7d2fc7425ec8f77b0c3a0f
SYED <abul.syed@oup.com>
2021-01-11 10:22:00 +0000

    format

--- src/views/playlist/CreatePlaylist.vue
+++ src/views/playlist/CreatePlaylist.vue
@@ -3,7 +3,7 @@
     <h3>Create new playlist</h3>
     <input type="text" required placeholder="Playlist title" v-model="title">
     <textarea required placeholder="Playlist description" v-model="description"></textarea>
-    <label>Upload playlist image</label>
+    <label>Upload playlist cover image</label>
     <input type="file" @change="handleChange">
     <div class="error">{{ fileError }}</div>
     <button>Create</button>
@@ -36,7 +36,7 @@ export default {
         fileError.value = null
       }else{
         file.value = null
-        fileError.value = 'Please select image file (png or jpg)'
+        fileError.value = 'Please select an image file (png or jpg)'
       }
     }
 
