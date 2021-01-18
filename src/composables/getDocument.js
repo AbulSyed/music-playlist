3134a3b835ea7ffd6d09ac773541543a9906ab79
SYED <abul.syed@oup.com>
2021-01-18 15:24:42 +0000

    playlist details

---
+++ src/composables/getDocument.js
@@ -0,0 +1,29 @@
+import { ref, watchEffect } from "vue"
+import { firestoreService } from "../firebase/config"
+
+const getDocument = (collection, id) => {
+  const document = ref(null)
+  const error = ref(null)
+
+  let documentRef = firestoreService.collection(collection).doc(id)
+
+  const unsub = documentRef.onSnapshot(snap => {
+    if(snap.data()){
+      document.value = { ...snap.data(), id: snap.id }
+      error.value = null
+    }else{
+      throw new Error('Document doesnt exist')
+    }
+  }, (e) => {
+    error.value = e.message
+    console.log(error.value)
+  })
+
+  watchEffect((onInvalidate) => {
+    onInvalidate(() => unsub())
+  })
+
+  return { document, error }
+}
+
+export default getDocument
--- src/views/playlist/PlaylistDetails.vue
+++ src/views/playlist/PlaylistDetails.vue
@@ -1,13 +1,67 @@
 <template>
-  <p>{{ id }}</p>
+  <div v-if="error">{{ error }}</div>
+  <div v-if="document" class="playlist-details">
+    <div class="playlist-info">
+      <div class="cover">
+        <img :src="document.coverUrl" alt="">
+      </div>
+      <h2>{{ document.title }}</h2>
+      <p class="name">Created by {{ document.name }}</p>
+      <p class="description">{{ document.description }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import getDocument from '../../composables/getDocument'
+
 export default {
-  props: ['id']
+  props: ['id'],
+  setup(props){
+    const { document, error } = getDocument('playlists', props.id)
+
+    return { document, error }
+  }
 }
 </script>
 
 <style>
-
+  .playlist-details {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    gap: 80px;
+  }
+  .cover {
+    overflow: hidden;
+    border-radius: 20px;
+    position: relative;
+    padding: 160px;
+  }
+  .cover img {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    min-width: 100%;
+    min-height: 100%;
+    max-width: 200%;
+    max-height: 200%;
+  }
+  .playlist-info {
+    text-align: center;
+  }
+  .playlist-info h2 {
+    text-transform: capitalize;
+    font-size: 28px;
+    margin-top: 20px;
+  }
+  .playlist-info p {
+    margin-bottom: 20px;
+  }
+  .name {
+    color: #999;
+  }
+  .description {
+    text-align: left;
+  }
 </style>
